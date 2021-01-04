f76cb0a089e2b2a66474b764b9dd4b7653e02f37
Phillip Smith <FilupErwinSmith@gmail.com>
2021-01-04 13:58:45 +1100

    Added exceptions

---
+++ MupenSharp/MupenSharp/Exceptions/InvalidFileVersionException.cs
@@ -0,0 +1,31 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MupenSharp
+// Project: MupenSharp
+// File Name: InvalidFileVersionException.cs
+// 
+// Current Data:
+// 2021-01-04 1:52 PM
+// 
+// Creation Date:
+// 2021-01-04 1:51 PM
+
+#endregion
+
+using System;
+
+namespace MupenSharp.Exceptions
+{
+  public class InvalidFileVersionException : Exception
+  {
+    public InvalidFileVersionException()
+    {
+    }
+
+    public InvalidFileVersionException(string message) : base(message)
+    {
+    }
+  }
+}
---
+++ MupenSharp/MupenSharp/Exceptions/InvalidFrameCountException.cs
@@ -0,0 +1,41 @@
+﻿#region Title Header
+
+// Name: Phillip Smith
+// 
+// Solution: MupenSharp
+// Project: MupenSharp
+// File Name: InvalidFrameCountException.cs
+// 
+// Current Data:
+// 2021-01-04 1:41 PM
+// 
+// Creation Date:
+// 2021-01-04 1:39 PM
+
+#endregion
+
+using System;
+
+namespace MupenSharp.Exceptions
+{
+  /// <summary>
+  ///   Exception for when a file's frame count and header frame count property do not match
+  /// </summary>
+  public class InvalidFrameCountException : Exception
+  {
+    /// <summary>
+    ///   Raises an exception to indicate there is an invalid frame count
+    /// </summary>
+    public InvalidFrameCountException()
+    {
+    }
+
+    /// <summary>
+    ///   Raises an exception to indicate there is an invalid frame count
+    /// </summary>
+    /// <param name="message"></param>
+    public InvalidFrameCountException(string message) : base(message)
+    {
+    }
+  }
+}
